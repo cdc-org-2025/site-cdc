d8a20a2a090e959b43cf91be16eb8cb0653a814b
cdc-org-2025 <comunicacao@cdc.org.br>
2025-04-13 23:24:13 -0300

    implementando config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,8 @@ const withNextIntl = require('next-intl/plugin')('./next-intl.config.ts');
 module.exports = withNextIntl({
   // Outras configs do Next.js
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+//   experimental: {
+//     appDir: true
+//   }
 });
 
---
+++ frontend/next.config.test.mjs
@@ -0,0 +1,19 @@
+// 
+// import createNextIntlPlugin from 'next-intl/plugin';
+
+// const withNextIntl = createNextIntlPlugin();
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   env: {
+//     baseUrlDomain: process.env.NEXT_PUBLIC_API_BASE_DOMAIN
+//   },
+//   images: {
+//     remotePatterns: [{ hostname: 'flagcdn.com' }],
+//   }
+// };
+
+// export default withNextIntl(nextConfig);
+
+
