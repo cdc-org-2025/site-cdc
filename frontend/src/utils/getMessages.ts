6ab2334d2c8c7888bea4a4e5c0ab6f0fbbebbb86
cdc-org-2025 <comunicacao@cdc.org.br>
2025-04-14 03:17:13 -0300

    fix: correcao producao

--- frontend/src/utils/getMessages.ts
+++ frontend/src/utils/getMessages.ts
@@ -1,5 +1,9 @@
-import { getMessages } from 'next-intl/server';
-
-export async function fetchMessages(locale?: string | any) {
-  return await getMessages(locale);
+export async function fetchMessages(locale: string) {
+  try {
+    const messages = await import(`../messages/${locale}.json`)
+    return messages.default
+  } catch (error) {
+    console.error(error)
+    return undefined
+  }
 }
