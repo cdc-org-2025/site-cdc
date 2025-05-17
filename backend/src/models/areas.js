4eb72a8054accc6001b492cc71d21f0a309ecbf3
cdc-org-2025 <comunicacao@cdc.org.br>
2025-05-17 19:16:05 -0300

    fix: alterando para salvar mais de uma area em liderança

--- backend/src/models/areas.js
+++ backend/src/models/areas.js
@@ -13,7 +13,7 @@ class Area extends Model {
   }
 
   static associate(models) {
-    this.hasMany(models.Lideranca, { foreignKey: 'area_id' });
+    // this.hasMany(models.Lideranca, { foreignKey: 'area_id' });
     this.hasMany(models.Programa, { foreignKey: 'area_id' });
     this.hasMany(models.Publicacao, { foreignKey: 'area_id' });
     this.hasMany(models.Transparencia, { foreignKey: 'area_id' });
--- backend/src/models/lideranca.js
+++ backend/src/models/lideranca.js
@@ -1,4 +1,27 @@
 
+// import { DataTypes, Model } from "sequelize";
+
+// class Lideranca extends Model {
+//   static init(sequelize) {
+//     return super.init({
+//       nome: DataTypes.STRING,
+//       cargo: DataTypes.STRING,
+//       email: DataTypes.STRING,
+//       url_imagem: DataTypes.STRING,
+//     }, {
+//       sequelize,
+//       tableName: "lideranca",
+//       timestamps: false,
+//     });
+//   }
+
+//   static associate(models) {
+//     this.belongsTo(models.Area, { foreignKey: "area_id" });
+//   }
+// }
+
+// export default Lideranca;
+
 import { DataTypes, Model } from "sequelize";
 
 class Lideranca extends Model {
@@ -8,6 +31,11 @@ class Lideranca extends Model {
       cargo: DataTypes.STRING,
       email: DataTypes.STRING,
       url_imagem: DataTypes.STRING,
+
+      area_ids: {
+        type: DataTypes.ARRAY(DataTypes.INTEGER), // <- Aqui está a mágica
+        allowNull: true,
+      },
     }, {
       sequelize,
       tableName: "lideranca",
@@ -16,7 +44,11 @@ class Lideranca extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.Area, { foreignKey: "area_id" });
+    // this.belongsToMany(models.Area, {
+    //   through: "area_lideranca",
+    //   foreignKey: "lideranca_id",
+    //   otherKey: "area_id"
+    // });
   }
 }
 
