8f754ee8120fe64cbbd9a8eb47b7daeb1b3b53c9
cdc-org-2025 <comunicacao@cdc.org.br>
2025-05-22 17:43:26 -0300

    fix: correcao ordem linha do tempo

--- backend/src/controllers/LinhaDoTempoController.js
+++ backend/src/controllers/LinhaDoTempoController.js
@@ -1,19 +1,23 @@
 
 import db from "../models/index.js";
+import { Sequelize } from 'sequelize'
 
 class LinhaTempoController {
   static async index(req, res) {
     try {
-      const dadosBancarios = await db.LinhaDoTempo.findAll({
+      const linhadotempo = await db.LinhaDoTempo.findAll({
         include: [{
           model: db.LinhaDoTempoImagem,
           as: 'imagens',
-          attributes: ['url_imagem'], // Só queremos as URLs
+          attributes: ['url_imagem'],
         }],
+        order: [
+          [Sequelize.literal(`CASE WHEN ano = 0 THEN 1 ELSE 0 END`), 'ASC'], // Move ano=0 pro final
+          ['ano', 'ASC']
+        ]
       });
 
-      // Formatar para retornar apenas array de urls
-      const response = dadosBancarios.map(item => ({
+      const response = linhadotempo.map(item => ({
         id: item.id,
         titulo: item.titulo,
         ano: item.ano,
