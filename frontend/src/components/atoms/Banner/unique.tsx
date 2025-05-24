491d917a1adae1c3f1786d8a4b9f96e9335773e9
site-cdc <comunicacao@cdc.org.br>
2025-05-24 01:23:21 -0300

    chore: remove

--- frontend/src/components/atoms/Banner/unique.tsx
+++ frontend/src/components/atoms/Banner/unique.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import { storageUrl } from '@/constants/storageDomain'
 
 export interface IBannerUnique {
   Banner: TypeBannerUnique
