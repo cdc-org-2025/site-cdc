63199a1ff1a5da75b45423d9e3bc29307853b28a
cdc-org-2025 <comunicacao@cdc.org.br>
2025-05-27 20:53:08 -0300

    fix: build cors

--- backend/src/routes/contatoForm.js
+++ backend/src/routes/contatoForm.js
@@ -1,21 +1,11 @@
+// routes/contato.js
 import { Router } from "express";
 import ContatoFormController from "../controllers/ContatoFormController.js";
-// import cors from 'cors';
+import multer from 'multer';
 
 const router = Router();
+const upload = multer();
 
-// const corsOptions = {
-//   origin: '*', // Substitua por um domínio específico em produção!
-//   methods: ['POST'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// };
-
-// // ✅ Responde ao preflight (OPTIONS) com CORS permitido
-// router.options('/', cors(corsOptions));
-
-// ✅ Rota principal POST
-// router.post('/', cors(corsOptions), ContatoFormController.index);
-router.post('/', ContatoFormController.index);
-
+router.post('/', upload.none(), ContatoFormController.index);
 
 export default router;
