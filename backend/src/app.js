c458f1532b8e20cb17c7a9ba8157022312fbff47
cdc-org-2025 <comunicacao@cdc.org.br>
2025-05-27 20:33:58 -0300

    fix: correcao cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,13 +8,25 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
+// const corsOptions = {
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// };
+// No seu app Express (antes das rotas)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://site-cdc.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+  
+  next();
+});
+// app.use(cors(corsOptions));
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", routes);
 
