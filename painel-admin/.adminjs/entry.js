d554b4aafeeea8c6de6aaf1c538e183be3347e37
cdc-org-2025 <comunicacao@cdc.org.br>
2025-06-04 14:55:12 -0300

    fix: correcao componentes

--- painel-admin/.adminjs/entry.js
+++ painel-admin/.adminjs/entry.js
@@ -21,6 +21,8 @@ import EditorLinhaTempo from '../src/components/editorlinhaTempo'
 AdminJS.UserComponents.EditorLinhaTempo = EditorLinhaTempo
 import ProgramaEditor from '../src/components/editorProgramas'
 AdminJS.UserComponents.ProgramaEditor = ProgramaEditor
+import TextoPreview from '../src/components/textoShow'
+AdminJS.UserComponents.TextoPreview = TextoPreview
 import UploadEditComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadEditComponent'
 AdminJS.UserComponents.UploadEditComponent = UploadEditComponent
 import UploadListComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadListComponent'
