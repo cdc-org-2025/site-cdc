6b6ee997e014c6b2c367bd77d3667520a68efb2b
cdc-org-2025 <comunicacao@cdc.org.br>
2025-06-17 10:03:09 -0300

    fix: correcao texto

--- painel-admin/models/conteudo_secao.js
+++ painel-admin/models/conteudo_secao.js
@@ -16,7 +16,9 @@ export class ConteudoSecao extends Model {
           'organizacao',
           'entrada-programas',
           'liderancas',
-          'transparencia'
+          'transparencia',
+          'indicadores',
+          'institucional'
         ),
         allowNull: false,
         unique: true, // se quiser garantir uma seção única
